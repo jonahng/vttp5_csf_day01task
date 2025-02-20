63c2ef6cbf8452cb346ef4c50bc0479c0b9f901b
jonahng <nyhj1998@gmail.com>
2025-02-20 15:34:26 +0800

    task can do counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,41 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'vttp5_csf_day01task';
+
+  increment = 1;
+  number = 12;
+  blankUrl = "numbers\\number";
+  fileExtension = ".jpg";
+  fileUrl = this.blankUrl + this.number + this.fileExtension;
+
+  imageUrl= "numbers\\number2.jpg";
+
+  changeIncrement(event : any){
+    console.log("THE EVENT TARGET VALUE:" + event.target.value);
+    this.increment = parseInt(event.target.value);
+    
+  }
+
+
+
+  previousClick(){
+    this.number-= this.increment;
+    if(this.number < 0){
+      this.number +=31;
+    }
+    console.log(this.number)
+    console.log(this.fileUrl)
+    this.fileUrl = this.blankUrl + this.number + this.fileExtension;
+  }
+
+  nextClick(){
+    this.number+= this.increment;
+    if(this.number > 30){
+      this.number-=31;
+    }
+    console.log(this.number)
+    console.log(this.fileUrl)
+    this.fileUrl = this.blankUrl + this.number + this.fileExtension;
+  }
+  
 }
